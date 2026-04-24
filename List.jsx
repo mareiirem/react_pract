c717d70167580c0933aa9a9c0f86a085af53e3a7
mareiirem <remlla.ali1@gmail.com>
2026-04-23 23:14:18 -0400

    Update List.jsx

--- List.jsx
+++ List.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 function List(props) {
     return (
         <ul>
-            {props.items.map((item) => (
-                <li key={item}>{item}</li>
+            {props.items.map((item, index) => (
+                <li key={index}>
+                    {typeof item === "string" ? item : item.name}
+                </li>
             ))}
         </ul>
     );
